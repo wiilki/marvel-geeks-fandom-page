7df1d5dc61c7a40f1c40d0b91c5980ae9e453fd4
Will <willkim0608@gmail.com>
2022-11-20 23:41:31 -0800

    Add fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,27 @@ var searchFormEl = document.querySelector('#search-form')
         };
     };
 
+    var getCharData = function (char) {
+        // Call api by char name first
+        var marvelApiUrl = 'http://gateway.marvel.com/v1/public/characters?ts=1&apikey=1a380943b10a057172bfb3d0c8676926&hash=d756b5220d6651f35ff1e5576f63f362';
 
+        // Fetch Marvel Data
+        fetch(marvelApiUrl)
+            .then(function (response) {
+                // Error if response cannot find char
+                if (response.ok) {
+                    // Parse info from api
+                    console.log(response.json())
+                    // .then(function (data) {
+                    //     getCharData(data, char);
+                    // })
+                } else {
+                    alert("Check your spelling");
+                }
+            }).catch(function (error) {
+                alert("Unable to connect");
+            });
+    };
     
     
     searchFormEl.addEventListener('submit', formSubmitHandler);
