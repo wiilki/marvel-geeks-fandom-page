405a4df684137cbe98a093214d4ac5cc413e4b90
Will <willkim0608@gmail.com>
2022-11-21 00:36:55 -0800

    Find character ID number

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ var searchFormEl = document.querySelector('#search-form')
 var characterArray = [];
 
 
-
 // Even handler for new character search
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -19,17 +18,22 @@ var formSubmitHandler = function (event) {
     };
 };
 
+// Match user input to characters name in array
 function getCharData(character) {
+    // Iterates through first level of arrays
     for (j = 0; j < characterArray.length; j++) {
         var newArray = characterArray[j];
+        // Iterates through second level of arrays
         for (k = 0; k < newArray.length; k++) {
             var charName = newArray[k].name;
+            var charIdNum = newArray[k].id;
+            // If value of input equals a character's name in the array
             if (charName.toUpperCase() === character.toUpperCase()) {
                 console.log(charName);
+                console.log(charIdNum);
             };
         };
     };
 };
-
 
 searchFormEl.addEventListener('submit', formSubmitHandler);
