9d2f4932feba3cf60cdf3de737e9106952f993b3
Will <willkim0608@gmail.com>
2022-11-28 20:01:30 -0800

    Add config.js file

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+var config = {
+    // git hub key
+    PUBLIC_KEY: '1a380943b10a057172bfb3d0c8676926',
+    PRIVATE_KEY: 'a37aff51e0de46803a79ee4b316c8b84b080ac21',
+    HASH_KEY: 'd756b5220d6651f35ff1e5576f63f362',
+    RAPID_KEY: '534da45c91msh1a199e3ff62abfdp1fed85jsn600cca5f4482'
+}
+
